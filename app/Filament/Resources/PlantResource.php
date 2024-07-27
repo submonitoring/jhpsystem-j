249038f8ae0c190c7b1f173fc81b7a3b3ad68a97
submonitoring <submonitoring@outlook.com>
2024-07-27 11:47:10 +0700

    Initial Plant Resource and Model

---
+++ app/Filament/Resources/PlantResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PlantResource\Pages;
+use App\Filament\Resources\PlantResource\RelationManagers;
+use App\Models\Plant;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PlantResource extends Resource
+{
+    protected static ?string $model = Plant::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPlants::route('/'),
+            'create' => Pages\CreatePlant::route('/create'),
+            'edit' => Pages\EditPlant::route('/{record}/edit'),
+        ];
+    }
+}
